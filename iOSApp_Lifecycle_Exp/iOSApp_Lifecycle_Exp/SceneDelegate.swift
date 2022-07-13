93c81a6a5d15dfeddd617c359336b4cc80b36a08
Shak49 <shakfeizi@gmail.com>
2022-07-13 11:08:01 -0500

    dark mode

--- iOSApp_Lifecycle_Exp/iOSApp_Lifecycle_Exp/SceneDelegate.swift
+++ iOSApp_Lifecycle_Exp/iOSApp_Lifecycle_Exp/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let vc = FirstViewController()
-        vc.view.backgroundColor = .white
+        //vc.view.backgroundColor = .white
         let navigation = UINavigationController(rootViewController: vc)
         window.rootViewController = navigation
         self.window = window
